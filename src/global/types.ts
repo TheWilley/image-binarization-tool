745ce342db22fd84cb509e2d95f19aac0182d9a7
thewilley <89783791+TheWilley@users.noreply.github.com>
2025-12-15 20:45:06 +0100

    refactor: remove unused comment and typescript interface

--- src/global/types.ts
+++ src/global/types.ts
@@ -3,7 +3,5 @@ export type Setter<T> = React.Dispatch<React.SetStateAction<T>>;
 export interface RLEData {
   width: number;
   height: number;
-  runs: number[]; // Array representing run lengths
+  runs: number[];
 }
-
-export type GridState = (0 | 1)[]; // Represents binary grid state
