3db30d00a2eaeba098a0849f849f4c6ed72a1e13
thewilley <89783791+TheWilley@users.noreply.github.com>
2026-01-03 12:45:20 +0100

    chore: seperate react and image-js into seperate js files in rollup config to reduce large chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,28 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   base: '/image-binarization-tool/',
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // React core
+            if (
+              id.includes('react') ||
+              id.includes('react-dom') ||
+              id.includes('react-router')
+            ) {
+              return 'react-vendor';
+            }
+            // ImageJS library
+            if (id.includes('image-js')) {
+              return 'image-js-vendor';
+            }
+            // Everything else in node_modules
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
