49f9779fd7a9b0b164e7a0b1c32d6fb711785a37
thewilley <89783791+TheWilley@users.noreply.github.com>
2026-01-04 11:45:10 +0100

    refactor: move encoded data display to ActionCollapse component

--- src/components/ui/tabs/encodeTab/EncodedData.tsx
+++ src/components/ui/tabs/encodeTab/EncodedData.tsx
@@ -1,62 +1,25 @@
 import { useState } from 'react';
+import CopyIcon from '../../../../icons/CopyIcon';
+import ActionCollapse from '../../../shared/ActionCollapse';
 
 export default function EncodedData({ encodedData }: { encodedData: string }) {
-  const [copied, setCopied] = useState(false);
+  const [copied, setCopied] = useState('Copy');
 
   const handleCopy = () => {
     navigator.clipboard.writeText(encodedData).then(() => {
-      setCopied(true);
-      setTimeout(() => setCopied(false), 2000);
+      setCopied('Copied!');
+      setTimeout(() => setCopied('Copy'), 2000);
     });
   };
 
   return (
-    <>
-      <div
-        tabIndex={0}
-        className='collapse collapse-arrow bg-[#0a0a0a] border rounded-lg border-base-300 w-full'
-      >
-        <div className='collapse-title font-semibold'>Show Encoded Data</div>
-        <div className='collapse-content text-sm'>
-          <button className='btn' onClick={handleCopy}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='size-6'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5A3.375 3.375 0 0 0 6.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0 0 15 2.25h-1.5a2.251 2.251 0 0 0-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 0 0-9-9Z'
-              />
-            </svg>
-            {copied ? 'Copied!' : 'Copy'}
-          </button>
-          <div
-            className='tooltip tooltip-left float-right cursor-pointer p-1'
-            data-tip='This is the RLE encoded binary data of the thresholded image.'
-          >
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              strokeWidth={1.5}
-              stroke='currentColor'
-              className='size-6'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                d='M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z'
-              />
-            </svg>
-          </div>
-          <textarea className='textarea w-full font-mono' value={encodedData} readOnly />
-        </div>
-      </div>
-    </>
+    <div className='flex flex-col gap-2'>
+      <ActionCollapse
+        label='Show Encoded Data'
+        data={encodedData}
+        tooltip='This is the RLE encoded binary data of the thresholded image.'
+        actionButtons={[{ text: copied, action: handleCopy, icon: <CopyIcon /> }]}
+      />
+    </div>
   );
 }
